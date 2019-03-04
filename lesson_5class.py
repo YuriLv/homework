ac292e20e383b1fbb7474251ee548f0062a7d22c
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-03-05 00:27:48 +0200

    changed file 5 class

--- lesson_5class.py
+++ lesson_5class.py
@@ -1,21 +1,42 @@
- # a= (input("Input numbers : "))
-i=0
-d=[]
-a=[]
-while i < 5:
-
-    b= int(input("Input numbers : "))
-    a.append(b)
-    i=i+1
-# a = [12,3,45,78]
-b= sorted(a)
-print(b)
-max = max(a)
-min= min(a)
-
-print(max)
-print(min)
-
+#
+# i=0
+# a=[]
+# while i < 5:
+#
+#     b= int(input("Input numbers : "))
+#     a.append(b)
+#     i=i+1
+# # a = [12,3,45,78]
+# a = sorted(a)
+# print(a)
+# max = max(a)
+# min= min(a)
+#
+# print("max =", max)
+# print("min =", min)
+#
+# print("Variant 2----------------------------------------------------------")
+#
+#
+# a=[]
+# for i in range(5):
+#     b = int(input("Input number:"))
+#     a.append(b)
+# a = sorted(a)
+# print(a)
+# max1 = max(a)
+# min1 = min(a)
+# print("max =", max1)
+# print("min =", min1)
+#
+#
+#
+#
+#
+# print("Variant 3----------------------------------------------------------")
+#
+#
+#
 # i= int((input("Input numbers : ")))
 #
 # list1=[int(input("next number ")) for k in range(i)]
@@ -29,8 +50,8 @@ print("2. ----------------------------------------------------")
 b=[]
 c=[]
 d=[]
-a = range(1,11,1)
-for i in a:
+
+for i in range(1,11):
     if i%2 == 0:
         b.append(i)
     elif i%3 == 0:
@@ -39,13 +60,7 @@ for i in a:
     else:
         d.append(i)
 
-print("b=", b)
-print("c=", c)
-print(d)
+print("Parni", b)
+print("Ne Parni", c)
+print("Other", d)
 
-
-        # else:
-    #     d= not c and not b
-    #     print(d)
-# print("b=", b)
-# print("c=", c)
