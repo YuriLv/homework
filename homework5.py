b2caebb0817287496bd34e8658e2ed4fa1c338f6
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-03-06 12:47:01 +0200

    add file homework5.py

---
+++ homework5.py
@@ -0,0 +1,16 @@
+print("1. --------------------------------------------------------------------")
+
+'''Program the function distance(p1, p2) which returns the distance between the points p1 and p2 in n-dimensional space. p1 and p2 will be given as arrays.
+
+Your program should work for all lengths of arrays, and should return -1 if the arrays aren't of the same length or if both arrays are empty sets.'''
+
+x1=4
+x2=6
+y1=5
+y2=6
+p1=[x1,y1]
+p2=[x2,y2]
+
+
+print(p1)
+print(dist)
