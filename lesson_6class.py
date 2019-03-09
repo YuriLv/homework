dc13d12b9ec2c8a055fd0b6dc549d5d421b68f97
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-03-09 17:00:47 +0200

    changed 6

--- lesson_6class.py
+++ lesson_6class.py
@@ -64,14 +64,75 @@
 
 
 print('5. -----------------------------------------------------')
+#
+# arg=input("input number: ")
+#
+# def suma(arg):
+#     s=0
+#     for i in range(len(arg)):
+#         i=int(arg[i])
+#         s=i+s
+#
+#     return s
+# print("Total suma of", arg, "is", suma(arg))
 
-arg=input("input number: ")
+print('6. -----------------------------------------------------')
+#
+# a=int(input("input  a="))
+# b=int(input("input  b="))
+# # a=5
+# # b=6
+# inp=input("input +, -, * or / :")
+#
+# def s(a,b):
+#     s=a+b
+#     print("a+b=",s)
+# def m(a,b):
+#     m=a-b
+#     print("a-b=", m)
+# def c(a,b):
+#     c=a/b
+#     print("a/b=", c)
+# def d(a,b):
+#     d=a*b
+#     print("a*b=", d)
+#
+# if inp=='+': s(a,b)
+# elif inp=='-': m(a,b)
+# elif inp =='*': d(a,b)
+# elif inp=="/": c(a,b)
+# else: print("Error operation !!!")
 
-def suma(arg):
-    s=0
-    for i in range(len(arg)):
-        i=int(arg[i])
-        s=i+s
 
-    return s
-print("Total suma of", arg, "is", suma(arg))
+print('7. -----------------------------------------------------')
+#
+# n=int(input("Enter n. Fibo number:"))
+# # n=11
+#
+# def f(n):
+#     a=1
+#     b=0
+#     for i in range(n):
+#         (a,b)=(b,a+b)
+#
+#     return b
+#
+#
+#
+# print("fibo =",f(n))
+
+
+print('8. -----------------------------------------------------')
+#
+# print("axx+bx+c")
+# print("D=bb-4ac")
+# a=int(input("input a:"))
+# b=int(input("input b:"))
+# c=int(input("input c:"))
+#
+# def Des(a,b,c):
+#     D=b*b-4*a*c
+#     return D
+# print("D=",Des(a,b,c))
+# D=Des(a,b,c)
+# print(D+26)
