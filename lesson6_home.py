b9a1f81bc7997f0265f391113f87834feccbc514
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-03-09 18:04:24 +0200

    changed file les6h

--- lesson6_home.py
+++ lesson6_home.py
@@ -1,10 +1,58 @@
 print('1. -----------------------------------------------------')
-'''Given an array of integers.
+# '''Given an array of integers.
+#
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+#
+# If the input array is empty or null, return an empty array.
+# Example
+#
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+# '''
+#
+# a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+#
+# s=0
+# m=0
+#
+# for i in range(len(a)):
+#
+#     if int(a[i])>0:
+#         s=s+1
+#     else:
+#         m=int(a[i])+m
+# # print([s,m])
+# l=[s,m]
+# print(l)
 
-Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+print('2. -----------------------------------------------------')
 
-If the input array is empty or null, return an empty array.
-Example
+'''In this kata you will create a function that takes in a list and returns a list with the reverse order.'''
 
-For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+arg=[1,3,5,6,76,3]
+
+def rev(arg):
+
+    arg.reverse()
+    return arg
+
+print(rev(arg))
+
+print(rev([3,1,5,4]))
+
+
+print('3. -----------------------------------------------------')
+'''If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+    Note: If the number is a multiple of both 3 and 5, only count it once.
 '''
+
+n=int(input("input number:"))
+# n=10
+s=0
+for i in range(n):
+    if i%3==0 or i%5==0:
+        s=s+i
+
+print("total sum is", s)
