6ee35f330f2ac417fbcec6001aeed622201c0b76
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-03-18 12:08:15 +0200

    added files home, class, less 8

---
+++ lesson8_home.py
@@ -0,0 +1,56 @@
+print('''1. ----------------------------------------------------''')
+'''Is the string uppercase?
+Task
+Create a method is_uppercase() to see whether the string is ALL CAPS. For example:
+is_uppercase("c") == False
+is_uppercase("C") == True
+is_uppercase("hello I AM DONALD") == False
+is_uppercase("HELLO I AM DONALD") == True
+is_uppercase("ACSKLDFJSgSKLDFJSKLDFJ") == False
+is_uppercase("ACSKLDFJSGSKLDFJSKLDFJ") == True'''
+
+
+def is_uppercase(text):
+
+    if text.isupper():
+        res = True
+
+    else:
+        res = False
+
+    return res
+
+text= "ACSKLDFJSgSKLDFJSKLDFJ"
+print("is_uppercase"+ "("+ text + ")" + " == ", is_uppercase(text))
+
+text= "hello I AM DONALD"
+print("is_uppercase"+ "("+ text + ")" + " == ", is_uppercase(text))
+
+text= "HELLO I AM DONALD"
+print("is_uppercase"+ "("+ text + ")" + " == ", is_uppercase(text))
+
+print("is_uppercase"+ "("+ text + ")" + " == ", is_uppercase(text))
+
+# text= input("Input text:")
+# print("is_uppercase"+ "("+ text + ")" + " == ", is_uppercase(text))
+
+
+print('''2. ----------------------------------------------------''')
+'''HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order!
+ Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+The sorting should NOT be case sensitive
+'''
+a=[]
+a.sort()
+print(a)
+print("Java????  Not clear what should to do")
+
+
+print('''3. ---------------------------------------------------- ''')
+'''You're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
+Weekday Month Day, time e.g., Friday May 2, 7pm
+You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+Write a function, shortenToDate, that takes the Website date/time in its original string format, and returns the shortened format.
+Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
+'''
+
